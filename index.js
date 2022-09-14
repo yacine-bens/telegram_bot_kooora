86e89ced1a52539c72da5a2036f174a9e549eab6
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-09-14 23:23:06 +0100

    Fix URI (additional /)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { JSDOM } = jsdom;
 // Telegram bot
 const { TOKEN, SERVER_URL } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-const URI = `/webhook/${TOKEN}`;
+const URI = `webhook/${TOKEN}`;
 const WEBHOOK_URL = SERVER_URL + URI;
 
 const app = express();
