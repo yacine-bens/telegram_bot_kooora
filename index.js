a708b339a6aebba8ba3b373df95468e1cfc8c782
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-09-14 17:35:16 +0100

    Remove getMatches()

--- index.js
+++ index.js
@@ -64,8 +64,9 @@ app.post(URI, async (req, res) => {
 app.post(URI + '/respond', async (req, res) => {
     let chatId = req.body.chat_id;
     console.log('respond', chatId);
-    let matches = await getMatches();
-    let response_message = formatMatchesDetails(matches);
+    // let matches = await getMatches();
+    // let response_message = formatMatchesDetails(matches);
+    let response_message = 'from resond endpoint';
 
     await axios.post(`${TELEGRAM_API}/sendMessage`, {
         chat_id: chatId,
