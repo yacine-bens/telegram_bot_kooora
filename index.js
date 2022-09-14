d1bfc151750ff59e26f207d0a097de3b993d5dd4
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2022-09-14 16:54:44 +0100

    First version

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 // Telegram bot
 const { TOKEN, VERCEL_URL } = process.env;
@@ -20,12 +22,53 @@ app.get(`/setWebhook`, async (req, res) => {
     return res.send(response.data);
 })
 
+
+// app.get('/matches', async (req, res) => {
+//     let matches = await getMatches();
+//     return res.send(matches);
+// })
+
+
 // Receive messages
 app.post(URI, async (req, res) => {
+    console.log(req.body);
 
+    // Check if update is a message
+    if (!req.body.message || !req.body.message.text) return res.send();
+
+    const chatId = req.body.message.chat.id;
+    const messageText = req.body.message.text;
+
+    let response_message = '';
+
+    if (isBotCommand(req.body.message)) {
+        if(messageText != '/start' && messageText != '/matches') response_message = 'Please enter a valid bot command.';
+        if(messageText === '/matches'){
+            // Send "Please wait while we get todays matches..." message to user
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: chatId,
+                text: 'Please wait while we fetch today\'s matches...'
+            })
+            
+            let matches = await getMatches();
+            response_message = formatMatchesDetails(matches);
+        }
+    }
+
+    // Respond to user
+    if(response_message != ''){
+        await axios.post(`${TELEGRAM_API}/sendMessage`, {
+            chat_id: chatId,
+            text: response_message
+        })
+    }
+
+    // Respond to Telegram server
+    return res.send();
 })
 
 app.listen(process.env.PORT || 5000);
+
 
 function isBotCommand(msg) {
     if (msg.text.startsWith('/') && msg.entities) {
@@ -34,4 +77,61 @@ function isBotCommand(msg) {
         }
     }
     return false;
+}
+
+
+async function getMatches() {
+    let matches = [];
+
+    const res = await axios.get('https://www.kooora.com');
+
+    const { document } = (new JSDOM(res.data)).window;
+    let matchesScript = [...document.querySelectorAll('script')].filter(el => el.innerHTML.includes('match_box'))[0];
+    let matchesIDs = matchesScript.innerHTML.split('var match_box = new Array (')[1].split(')')[0].split('\n').map(el => el.length < 10 ? null : el.split(',')[1]).filter(e => e != null);
+    for (let matchId of matchesIDs) {
+        let match = await getMatch(matchId);
+        matches.push(match);
+    }
+
+    return matches;
+}
+
+
+async function getMatch(id) {
+    let res = await axios.get('https://www.kooora.com/?m=' + id + '&ajax=true');
+    let matchDetails = res.data.matches_list;
+    let time = matchDetails[6].split('@')[0];
+    let teamId1 = matchDetails[7];
+    let teamId2 = matchDetails[12];
+    let channels = matchDetails[19].split('~l|');
+    // remove first element (referees and commentary details ...)
+    channels.shift();
+    channels = channels.map(channel => { return { id: channel.split('|')[0], name: channel.split('|')[1] } });
+    let teamName1 = await getTeam(teamId1);
+    let teamName2 = await getTeam(teamId2);
+
+    const match = {
+        time: time,
+        team1: teamName1,
+        team2: teamName2,
+        channels: channels
+    };
+
+    return match;
+}
+
+
+async function getTeam(id) {
+    let res = await axios.get('https://www.kooora.com/?team=' + id);
+    let team = res.data.match(/var team_name_en = "(.*?)"/)[1];
+    return team;
+}
+
+
+function formatMatchesDetails(matches){
+    let message = '';
+    matches.forEach(match => {
+        message += `${match['time']}\n${match['team1']} -- ${match['team2']}\n\n`;
+    });
+    return message;
 }
